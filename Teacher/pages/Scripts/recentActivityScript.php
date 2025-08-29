f560cb8ce2adf603a1a690c80d2d5b093c5465fb
Almujim Hadjili <almujim.hadjili@gmail.com>
2025-08-29 16:16:01 +0800

    fix: Corrected link path for viewing latest student attempt in activity modal

--- Teacher/pages/Scripts/recentActivityScript.php
+++ Teacher/pages/Scripts/recentActivityScript.php
@@ -117,7 +117,7 @@
 
             // Add action button to view the latest attempt details
             modalActions.innerHTML = `
-            <a href="../Functions/viewStudentAttempt.php?attempt_id=${activity.latest_attempt_id}" class="ml-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+            <a href="../../pages/viewStudentAttempt.php?attempt_id=${activity.latest_attempt_id}" class="ml-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
                 View Latest Attempt
             </a>
             
